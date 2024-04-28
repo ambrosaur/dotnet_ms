545bc56ddbe4b33f19bc07c7c7565627e8ca53ac
ambrosaur <49253746+ambrosaur@users.noreply.github.com>
2024-04-28 17:46:58 -0500

    display list of pizzas

--- Pages/PizzaList.cshtml.cs
+++ Pages/PizzaList.cshtml.cs
@@ -1,3 +1,5 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,23 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        // This variable will hold a reference to a PizzaService object
+        private readonly PizzaService _service;
+
+        /** 
+            hold the list of pizzas 
+            default! - initialized later, so null safety checks aren't required
+        */
+        public IList<Pizza> PizzaList { get; set;} = default!;
+
+        public PizzaListModel(PizzaService service)
+        {
+            _service = service;
+        }
+
         public void OnGet()
         {
+            PizzaList = _service.GetPizzas();
         }
     }
 }
